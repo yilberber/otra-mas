fe561cdfbdb06754303c721cae04c2d7e221848e
yilberber <143672289+yilberber@users.noreply.github.com>
2023-10-15 19:24:21 -0500

    Delete CUARTA ACTIVIDAD DASH directory

--- CUARTA ACTIVIDAD DASH/backend/main.py
+++
@@ -1,9 +0,0 @@
-import dash 
-from dash import html,dcc
-import dash_bootstrap_components as dbc
-
-#importamos el backend
-from backend.calculosondeos import clasificación_categorias
-
-layout= clasificación_categorias
-
